de10ede0c97dbfd2ccf12e10c7494ea06f4205c0
aapa96 <aapa96@gmail.com>
2016-04-14 00:46:59 -0500

    Pila

---
+++ Pilas/Pila.h
@@ -0,0 +1,107 @@
+/*
+ * Pila.h
+ *
+ *  Created on: 6 de abr. de 2016
+ *      Author: Alex Andre
+ */
+
+#ifndef PILA_H_
+#define PILA_H_
+
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <string>
+using namespace std;
+
+template<class T>
+class Nodo{
+public:
+	T Dato;
+	Nodo<T> *ant;
+
+    Nodo(T D)
+    {
+       Dato = D;
+       ant =0;
+    }
+};
+
+
+template<class T>
+class Pila {
+public:
+	Pila(){
+			Last=0;
+		}
+	Nodo<T> *Last;
+
+	int Tam;
+
+	bool istmpy(){
+			if(Last==0)
+				return true;
+			else
+				return false;
+		}
+	int tamanio(){
+		return Tam;
+	}
+
+	void push(T d){
+		Nodo<T> *pNuevo = new Nodo<T> (d);
+		if(Last == 0){
+			Last = pNuevo;
+			Tam = 1;}
+		else{
+			pNuevo->ant=Last;
+			Last = pNuevo;
+			Tam++;
+		}
+	}
+
+	T Top(){
+		return Last->Dato;
+	}
+
+	void Pop(){
+		if(istmpy())
+			return;
+		else
+		{
+			Nodo<T> *tmp = Last->ant;
+			delete Last;
+			Last= tmp;
+			Tam--;
+		}
+	}
+	void Show(){
+		Nodo<T>*Tmp=Last;
+		for(int i = 0;i<tamanio();i++){
+			cout<<Tmp->Dato<<endl;
+			Tmp = Tmp->ant;
+		}
+	}
+
+
+	void printDot(const char* p){
+		ofstream file(p);
+		Nodo<T>*Tmp=Last;
+		file<<"digraph {"<<endl;
+		while(Tmp->ant != 0){
+			file<<Tmp->Dato<<"->";
+			Tmp = Tmp->ant;
+		}
+		file<<Tmp->Dato<<endl;
+
+
+		file<<" }"<<endl;
+ 	}
+};
+
+
+
+
+
+
+#endif /* PILA_H_ */
